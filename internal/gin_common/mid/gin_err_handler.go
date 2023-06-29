be0dfdaeff99b4bc4bdcdb4cd29cc639f1f04bc8
hongjun500 <bhj.hong@qq.com>
2023-06-29 11:11:42 +0800

    全局的未知错误处理

---
+++ internal/gin_common/mid/gin_err_handler.go
@@ -0,0 +1,25 @@
+// @author hongjun500
+// @date 2023/6/29 10:11
+// @tool ThinkPadX1隐士
+// Created with 2022.2.2.IntelliJ IDEA
+// Description: 全局错误处理中间件
+
+package mid
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hongjun500/mall-go/internal/gin_common"
+)
+
+func ErrorMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				gin_common.CreateFail(c, gin_common.UnknownError)
+				c.Abort()
+				return
+			}
+		}()
+		c.Next()
+	}
+}
